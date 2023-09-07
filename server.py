46bd60503f4f67e3de39448b0e5b7f5a4f0fae7e
ksananth4424 <ksananth4424@gmail.com>
2023-09-08 01:31:02 +0530

    Server and client side

--- server.py
+++ server.py
@@ -9,5 +9,45 @@ server.bind((HOST, PORT))
 
 server.listen()
 
-clints = []
+clients = []
 nicknames = []
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            print(f"{nicknames[clients.index(client)]}")
+            broadcast(message)
+
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            nicknames.remove(nickname)
+            break
+
+def receive():
+    while(True):
+        client, address = server.accept()
+        print(f"Connected with {str(address)}!")
+
+        client.send("NICK".encode('utf-8'))
+        nickname = client.recv(1024)
+
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f"Nickname of the clint is {nickname}")
+        broadcast(f"{str(nickname)} connected to the server!\n".encode('utf-8'))
+        client.send('Connecteed to the server'.encode('utf-8'))
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+print("Server running")
+receive()
